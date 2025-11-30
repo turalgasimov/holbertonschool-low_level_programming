a25878cbae51dbba7e7baf81174098c7c4ed03b7
turalgasimov <turallgasimov@gmail.com>
2025-11-30 16:53:17 +0000

    yet another fix for task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * 
+ * Description: Prints the given string.
+ * @parameter: no parameters.
+ * 
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
