42ac22f1715f600cfd8e0e5ccc08ccc38c1ba895
turalgasimov <turallgasimov@gmail.com>
2025-11-30 17:22:51 +0000

    task 6 fix

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -2,17 +2,17 @@
 
 /**
  * main - Entry point
- * Description: prints the size of various types on the computer it is compiled and run on.
+ * Description: prints the size of various types.
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	int a;
-   	long int b;
+	long int b;
 	long long int c;
-   	char d;
-   	float f;
+	char d;
+	float f;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d);
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
@@ -21,4 +21,3 @@ int main(void)
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
-
