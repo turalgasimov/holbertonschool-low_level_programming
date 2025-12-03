54341acb2c34474def1494e47b85ac88056bffe9
turalgasimov <turallgasimov@gmail.com>
2025-12-03 18:09:14 +0000

    task 102 fix

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (i = 0; i <= 99; i++)
 	{
-		for (j = i + 1; j < 99; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
 			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
@@ -20,7 +20,7 @@ int main(void)
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
 
-			if (i < 98 || j < 99)
+			if (i != 98 || j != 99)
 			{
 				putchar(',');
 				putchar(' ');
