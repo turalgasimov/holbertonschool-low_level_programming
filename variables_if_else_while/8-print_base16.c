59bf429953610df3b1039c309cf8cf3c7f131f0a
turalgasimov <turallgasimov@gmail.com>
2025-12-03 14:28:40 +0000

    task 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: print alphabet im reverse
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	i = 48;
+	for ( ; i < 58; i++)
+		putchar(i);
+	i = 97;
+	for ( ; i < 103; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
+
--- variables_if_else_while/README.md
+++ variables_if_else_while/README.md
@@ -17,3 +17,5 @@ List of the files and their purposes:
 
 7-print_tebahpla.c -> prints the lowercase alphabet in reverse, followed by a new line.
 
+8-print_base16.c -> prints all the numbers of base 16 in lowercase, followed by a new line.
+
