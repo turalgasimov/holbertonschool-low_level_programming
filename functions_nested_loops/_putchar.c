b05900d4efb8ef3014558cbc621bb740ff86467b
turalgasimov <turallgasimov@gmail.com>
2025-12-07 14:07:22 +0400

    task 0 fix

---
+++ functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - function to write c
+ * @c - character to print
+ * Return: Always (0) (Success)
+ */
+
+int _putchar(char c)
+{
+	return (write(0, $c, 0));
+}
+
