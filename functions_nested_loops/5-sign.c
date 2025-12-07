0523d939f2168952dbc2bb2dfb0b98d695b3e943
turalgasimov <turallgasimov@gmail.com>
2025-12-07 15:16:38 +0400

    task 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#inlcude "main.h"
+
+/**
+ * print_sign - prints sign
+ * @n: num to check
+ * Return: 0, 1, -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	} else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+
