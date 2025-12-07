ce186cf6f48d5323c62a438cb69443d16fa9ca87
turalgasimov <turallgasimov@gmail.com>
2025-12-07 11:34:49 +0000

    task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - find abs value
+ * @res: num to check
+ * Return: abs value
+ */
+
+int _abs(int res);
+{
+	if (res >= 0)
+		return (res);
+	else
+		return (-res);
+}
