39839599e23eb78e5fe2762e9f8965cc144a002c
turalgasimov <turallgasimov@gmail.com>
2025-12-07 15:44:33 +0000

    task 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line in the terminal
+ * @n: number of times the character _ is printed
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+			_putchar('_');
+	}
+
+	_putchar('\n');
+}
+
