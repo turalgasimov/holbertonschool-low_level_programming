5648260bc4492dc14bebe1eeb1b14bb4f55bcc5e
turalgasimov <turallgasimov@gmail.com>
2025-12-07 13:44:33 +0000

    task 10 fix

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -9,19 +9,5 @@
 
 int add(int a, int b)
 {
-	int sum;
-
-	sum = a + b;
-	if (sum < 0)
-	{
-		_putchar('-');
-		sum = -sum;
-	}
-	if (sum >= 10)
-	{
-		_putchar((sum / 10) + '0');
-	}
-	_putchar((sum % 10) + '0');
-	_putchar('\n');
+	return (a + b);
 }
-
