16b2b062babb9b8e57b9a402b79faee7bb8a931e
turalgasimov <turallgasimov@gmail.com>
2025-12-07 14:33:32 +0400

    task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10x
+ * return 0
+ */
+
+void print_alphabet_x10(void)'
+{
+	int i;
+
+	i = 0;
+	for ( ; i < 10; i++)
+	{
+		print_alphabet();
+		_putchar('\n');
+	}
+
