bacbe261c41a81d50e619e107d7adab7294e0c2d
turalgasimov <turallgasimov@gmail.com>
2025-12-07 12:11:43 +0000

    task 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * times_table - 9 times table
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+		for (b = 0; b < 10; b++)
+		{
+			_putchar(a + '0');
+			_putchar(',');
+			_putchar(' ');
+			if (b == 9)
+				_putchar('\n');
+		}
+	_putchar('\n');
+}
