09d38f8b064a4a03df0c85de0e1591539ff10d49
turalgasimov <turallgasimov@gmail.com>
2025-12-07 15:35:15 +0000

    task 4 fix

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -11,9 +11,8 @@ void print_most_numbers(void)
 
 	while (i < 10)
 	{
-		if (i == 2 || i == 4)
-			continue;
-		_putchar(i + '0');
+		if (i != 2 || i != 4)
+			_putchar(i + '0');
 		i++;
 	}
 	_putchar('\n');
