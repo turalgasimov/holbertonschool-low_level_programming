ba5f3d10af1eff3a3f1f29383338b8cbbe572b8e
turalgasimov <turallgasimov@gmail.com>
2025-12-11 08:24:42 +0000

    task 6-puts2.c fix

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -8,9 +8,12 @@
 
 void puts2(char *str)
 {
-        while (*str != '\0')
-        {
-                _putchar(*str);
-                str += 2;
-        }
+	while (true)
+	{
+		_putchar(*str);
+		if (*str == '\0')
+			break;
+		str += 2;
+	}
 }
+
