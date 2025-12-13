e455ba0c481b3acec565784770f076e37a03f663
turalgasimov <turallgasimov@gmail.com>
2025-12-13 22:25:52 +0000

    task 100-atoi.c fix

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -20,7 +20,7 @@ int _atoi(char *s)
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
 			found = 1;
-			result = result * 10 + (s[i] - '0');
+			result = result * 10 - (s[i] - '0');
 		}
 		else if (found)
 			break;
