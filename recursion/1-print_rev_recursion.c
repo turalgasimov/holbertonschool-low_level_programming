375b95ff536c8e6fef465e46cb1351dc48ac7e28
turalgasimov <turallgasimov@gmail.com>
2025-12-17 21:27:24 +0000

    task 1-print_rev_recursion.c

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -10,9 +10,9 @@ void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar(s - 1);
+		_putchar(*(s - 1));
 		return;
 	}
 
-	_print_rev_recursion(s + 1);
+	_print_rev_recursion((*s + 1));
 }
