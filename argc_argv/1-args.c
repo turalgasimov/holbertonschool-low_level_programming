ca354619f7ab47e255c5c57bfc1f86cac7e98884
turalgasimov <turallgasimov@gmail.com>
2025-12-18 01:44:44 +0000

    task 1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: num of args
+ * @argv: vector of args
+ * Return: 0
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
--- argc_argv/README.md
+++ argc_argv/README.md
@@ -1,4 +1,6 @@
 List of the files and their purposes:
 
-0-whatsmyname.c -> rints its name, followed by a new line.
+0-whatsmyname.c -> prints its name, followed by a new line.
+
+1-args.c -> prints the number of arguments passed into it.
 
