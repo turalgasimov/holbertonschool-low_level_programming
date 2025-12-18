05c985c4c1041648d46a1954a83c3421b3e60774
turalgasimov <turallgasimov@gmail.com>
2025-12-18 01:38:29 +0000

    task 0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: num of args
+ * @argv: vector of args
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+        printf("./%s", argv[0]);
+        return (0);
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,4 @@
+List of the files and their purposes:
+
+0-whatsmyname.c -> rints its name, followed by a new line.
+
