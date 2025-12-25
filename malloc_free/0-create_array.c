cbdb60e2c7e4b0e0e7a0800efb4db93d4532aa1b
turalgasimov <turallgasimov@gmail.com>
2025-12-25 14:15:16 +0000

    task 0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,5 +1,5 @@
-#include "main.h";
-#include <stdlib.h>;
+#include "main.h"
+#include <stdlib.h>
 
 /**
  * create_array - entry point
@@ -11,7 +11,7 @@
 char *create_array(unsigned int size, char c)
 {
 	char *a;
-	int i;
+	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
