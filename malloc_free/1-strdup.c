59f4294eb512d41a4e402cf9966150393a89186c
turalgasimov <turallgasimov@gmail.com>
2025-12-25 14:30:03 +0000

    task 1-strdup.c

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - entry point
+ * @str: char
+ * Return: ptr / NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *a;
+	int size;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (*str != '\0')
+	{
+		str++;
+		size++;
+	}
+
+	str -= size;
+
+	a = malloc(size);
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		a[i] = str[i];
+
+	return (a);
+}
--- malloc_free/README.md
+++ malloc_free/README.md
@@ -2,3 +2,5 @@ List of the files and their purposes:
 
 0-create_array.c ->  creates an array of chars, and initializes it with a specific char.
 
+1-strdup.c -> returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.
+
