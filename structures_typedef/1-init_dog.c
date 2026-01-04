32e7175eeab19b6358aa2651f657667ed49c69f1
turalgasimov <turallgasimov@gmail.com>
2026-01-04 11:40:39 +0000

    task 1-init_dog.c

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "dog."
+#include <stdio.h>
+
+/**
+ * init_dog - entry point
+ * @d: struct
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
+
--- structures_typedef/README.md
+++ structures_typedef/README.md
@@ -2,3 +2,5 @@ List of the files and their purposes:
 
 dog.h -> dog struct
 
+1-init_dog.c -> initialize a variable of type struct dog.
+
