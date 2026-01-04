3dc2e8fb9f65dd49ab526adcaa545e8e1b8754de
turalgasimov <turallgasimov@gmail.com>
2026-01-04 16:30:53 +0000

    task 0-print_name.c

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name using a function pointer
+ * @name: name to print
+ * @f: pointer to function that prints a name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
---
+++ function_pointers/README.md
@@ -0,0 +1,4 @@
+List of the files and their purposes:
+
+0-print_name.c -> prints a name.
+
