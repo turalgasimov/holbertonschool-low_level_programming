9959c77b1f9838f692c47d1df8d55611e63ee9bb
turalgasimov <turallgasimov@gmail.com>
2026-01-09 08:58:05 +0000

    task 1-print_numbers.c

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d", va_arg(args, int));
 
 		if (separator != NULL && i != n - 1)
-			printf("%s ", separator);
+			printf("%s", separator);
 	}
 
 	printf("\n");
