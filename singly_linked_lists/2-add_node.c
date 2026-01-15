3b98d4d73ccd1b04d006167ccc49708daceb4f2f
turalgasimov <turallgasimov@gmail.com>
2026-01-15 15:49:56 +0000

    task 2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -13,6 +13,7 @@ list_t *add_node(list_t **head, const char *str)
 	new = (list_t *)malloc(sizeof(list_t));
 
 	new->str = strdup(str);
+	new->len = strlen(str);
 	new->next = *head;
 
 	*head = new;
