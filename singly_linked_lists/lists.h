9ac87222949e2e6b4539cd29251f5f001e7dc9eb
turalgasimov <turallgasimov@gmail.com>
2026-01-15 15:11:06 +0000

    task 0-print_list.c

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -5,12 +5,12 @@
 #include <stdlib.h>
 #include <stddef.h>
 
-typedef struct l
-{
-        char *str;
-        unsigned int len;
-        struct l *next;
-} list_t;
+// typedef struct l
+// {
+// 	char *str;
+// 	unsigned int len;
+// 	struct l *next;
+// } list_t;
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
