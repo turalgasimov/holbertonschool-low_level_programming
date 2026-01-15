46313edd6d4cdd3b13b21acba2617eaecc884981
turalgasimov <turallgasimov@gmail.com>
2026-01-15 12:34:12 +0000

    task 0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,13 +8,13 @@
 
 size_t print_list(const list_t *h)
 {
-	list_t *lst = h;
+	list_t lst = h;
 	size_t i = 0;
 	char *s;
 
-	while (lst->next != NULL)
+	while (lst.next != NULL)
 	{
-		*s = lst->str;
+		*s = lst.str;
 		if (s == NULL)
 			*s = "(nil)";
 		printf("[%d] %s", sizeof(*s), *s);
