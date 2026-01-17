10eec7ca6ff707fec2a3a45e1fb1f5506adfd5a9
turalgasimov <turallgasimov@gmail.com>
2026-01-17 15:46:10 +0000

    task 8-delete_dnodeint.c

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
  * delete_dnodeint_at_index - entry
- * @h: pointer to pointer to head;
- * @idx: nth node
+ * @head: pointer to pointer to head;
+ * @index: nth node
  * Return: 1/-1
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
@@ -14,10 +14,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		if (!tmp)
-			return(1);
+			return (1);
 		*head = tmp->next;
 		free(tmp);
-		return (1);		
+		return (1);
 	}
 
 	for (i = 0; i < index; i++)
@@ -25,13 +25,11 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (!tmp)
 		return (-1);
-	else
-	{
-		if (tmp->next)
-			tmp->next->prev = tmp->prev;
-		if (tmp->prev)
-			tmp->prev->next = tmp->next;
-		free(tmp);
-		return (1);
-	}
+	if (tmp->next)
+		tmp->next->prev = tmp->prev;
+	if (tmp->prev)
+		tmp->prev->next = tmp->next;
+	free(tmp);
+	return (1);
+
 }
