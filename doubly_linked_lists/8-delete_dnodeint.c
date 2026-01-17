eb7f117ba76493a8fa0101ff3072771b58983525
turalgasimov <turallgasimov@gmail.com>
2026-01-17 15:48:14 +0000

    task 8-delete_dnodeint.c

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -15,7 +15,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		if (!tmp)
 			return (1);
-		*head = tmp->next;
+		if (tmp->next)
+			*head = tmp->next;
 		free(tmp);
 		return (1);
 	}
