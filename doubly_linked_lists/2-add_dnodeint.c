515242640f19347a7c6db712ded0da8287438447
turalgasimov <turallgasimov@gmail.com>
2026-01-17 10:49:00 +0000

    task 2-add_dnodeint.c

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -19,8 +19,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 
 	new->prev = NULL;
-	new->next = *head;
-	new->next->prev = *new;
+	new->next = head;
+	new->next->prev = new;
 	new->n = n;
 
 	return (new);
