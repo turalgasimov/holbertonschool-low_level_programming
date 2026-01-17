a88455112830e1ac1bec4cead0989945edb72c21
turalgasimov <turallgasimov@gmail.com>
2026-01-17 10:51:42 +0000

    task 2-add_dnodeint.c

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -14,8 +14,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new = (dlistint_t *)malloc(sizeof(dlistint_t));
 	if (new == NULL)
 	{
-		return (NULL);
 		free(new);
+		return (NULL);
 	}
 
 	new->prev = NULL;
