43642a605963fb34e319a7a97b3fd63c45e241e4
turalgasimov <turallgasimov@gmail.com>
2026-01-17 15:25:51 +0000

    task 7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->n = n;
 	if (idx == 0)
 	{
-		add_dnodeint(h, n);
+		new = add_dnodeint(h, n);
 		return (new);
 	}
 	tmp = *h;
