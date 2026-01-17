6a7f578bceb788c40cc397e216d79e05c67701c7
turalgasimov <turallgasimov@gmail.com>
2026-01-17 12:42:55 +0000

    task 4-free_dlistint.c

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -9,7 +9,7 @@ void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *tmp = head;
 
-	if ((head->next == NULL && head->prev == NULL)  || head == NULL)
+	if ((head == NULL) || (head->next == NULL && head->prev == NULL))
 	{
 		free(head);
 	} else
