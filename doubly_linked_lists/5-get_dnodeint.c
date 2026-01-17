441676660b0700b66362752b4f9cca04b38210a3
turalgasimov <turallgasimov@gmail.com>
2026-01-17 12:50:16 +0000

    task 5-get_dnodeint.c

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - entry
+ * @head: head
+ * @index: nth node
+ * Return: node
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *node = head;
+	int i;
+
+	for (i = 0; i < index; i++)
+	{
+		if (node == NULL)
+		{
+			break;
+		}
+
+		node = node->next;
+	}
+
+	return (node);
+}
--- doubly_linked_lists/main.c
+++ doubly_linked_lists/main.c
@@ -3,12 +3,21 @@
 int main() 
 {
         dlistint_t *head;
+        dlistint_t *node;
+
         head = NULL;
-
-        add_dnodeint_end(&head, 10);
-        add_dnodeint_end(&head, 22);
-        printf("-> %zu elements\n", print_dlistint(head));
-
+        add_dnodeint_end(&head, 0);
+        add_dnodeint_end(&head, 1);
+        add_dnodeint_end(&head, 2);
+        add_dnodeint_end(&head, 3);
+        add_dnodeint_end(&head, 4);
+        add_dnodeint_end(&head, 98);
+        add_dnodeint_end(&head, 402);
+        add_dnodeint_end(&head, 1024);
+        print_dlistint(head);
+        node = get_dnodeint_at_index(head, 5);
+        printf("%d\n", node->n);
         free_dlistint(head);
+        head = NULL;
         return (0);
 }
