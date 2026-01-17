7a2474a2accb11de4ba097b63a7d03ba413ab6f5
turalgasimov <turallgasimov@gmail.com>
2026-01-17 09:45:00 +0000

    task 0-print_dlistint.c

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - entry
+ * @h: head
+ * Return: int
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t cnt = 0;
+
+	if (h == NULL)
+		return (NULL);
+
+	while (h->next != NULL)
+	{
+		cnt++;
+
+		printf("%d\n", h->n);
+
+		h = h->next;
+	}
+
+	return (cnt);
+}
+
