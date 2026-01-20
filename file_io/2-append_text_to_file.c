ff76b081db838d4724aab6c1da5385c02e2ee7e1
turalgasimov <turallgasimov@gmail.com>
2026-01-20 13:20:26 +0000

    2-append_text_to_file.c

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - entry
+ * @filename: filename
+ * @text_content: string
+ * Return: 1/-1
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytesWrit;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		bytesWrit = write(fd, text_content, strlen(text_content));
+		if (bytesWrit == -1)
+			return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
--- file_io/main.c
+++ file_io/main.c
@@ -5,7 +5,8 @@ int main()
 	const char *filename = "test.txt";
 	char *text_content = "Saam Eekim";
 
-	create_file(filename, text_content);
+	int res = append_text_to_file(filename, text_content);
+	printf("%d\n", res);
 
 	return (0);
 }
