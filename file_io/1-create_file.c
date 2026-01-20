10873c382a4f1d7313df2d235ea7f282fd622bbe
turalgasimov <turallgasimov@gmail.com>
2026-01-20 13:08:55 +0000

    1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -3,8 +3,8 @@
 /**
  * create_file - entry
  * @filename: filename
- * @letters: num of letters
- * Return: num of letters / 0
+ * @text_content: string
+ * Return: 1/-1
  */
 
 int create_file(const char *filename, char *text_content)
