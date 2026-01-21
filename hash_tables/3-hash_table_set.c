a97732d5e0f18e904e5b64604f3478c18dd19670
turalgasimov <turallgasimov@gmail.com>
2026-01-21 15:05:56 +0000

    3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -5,13 +5,13 @@
  * @ht: hash table you want to add or update the key/value to.
  * @key: is the key.
  * @value: is the value associated with the key.
- * 
+ *
  * Return: 1 if it succeeded, 0 otherwise.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	
+
 	index = key_index(key, sizeof(ht));
 
 	hash_node_t *newNode = (hash_node_t *)malloc(sizeof(hash_node_t));
