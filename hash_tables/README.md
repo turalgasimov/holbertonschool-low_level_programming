736997a8091a3cd3b1eba3b28cb38c2d8d8de96c
turalgasimov <turallgasimov@gmail.com>
2026-01-21 14:16:21 +0000

    1-djb2.c

--- hash_tables/README.md
+++ hash_tables/README.md
@@ -2,3 +2,4 @@ List of the files and their purposes:
 
 0-hash_table_create.c -> creates a hash table.
 
+1-djb2.c -> implementing the djb2 algorithm.
--- hash_tables/main.c
+++ hash_tables/main.c
@@ -7,10 +7,14 @@
  */
 int main(void)
 {
-	hash_table_t *ht;
+	char *s;
 
-	ht = hash_table_create(1024);
-	printf("%p\n", (void *)ht);
-
+    	s = "cisfun";
+    	printf("%lu\n", hash_djb2((unsigned char *)s));
+    	s = "Don't forget to tweet today";
+    	printf("%lu\n", hash_djb2((unsigned char *)s));
+    	s = "98";
+    	printf("%lu\n", hash_djb2((unsigned char *)s));
+	
 	return (0);
 }
