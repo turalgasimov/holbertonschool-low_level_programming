186895e4c7ee558f8c7b2b9a8be5ec01b9416fb5
turalgasimov <turallgasimov@gmail.com>
2026-01-22 12:31:07 +0000

    3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -33,7 +33,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			if (!strcmp(key, tmp->key))
 			{
 				free(tmp->value);
-				tmp->value = value;
+				tmp->value = strdup(value);
 				ht->array[index] = newNode;
 				return (1);
 			}
