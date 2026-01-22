462cd207ffaede5e0a41f78deb0d126da1b3acc8
turalgasimov <turallgasimov@gmail.com>
2026-01-22 13:17:28 +0000

    4-hash_table_get.c

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -13,11 +13,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *node;
 	int index;
 
-	index = key_index((unsigned char *)key, ht->size);
-	node = ht->array[index];
-
 	if (!ht || !key)
 		return (NULL);
+
+	index = key_index((unsigned char *)key, ht->size);
+	node = ht->array[index];
 
 	while (node)
 	{
