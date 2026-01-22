5dcecf64e2accce66139b8826e7a868c08daa384
turalgasimov <turallgasimov@gmail.com>
2026-01-22 13:27:03 +0000

    5-hash_table_print.c

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -4,7 +4,7 @@
  * hash_table_print- prints a hash table.
  * @ht: is the hash table.
  */
-void hash_table_print(const hash_table_t *ht);
+void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *node;
 	int index;
